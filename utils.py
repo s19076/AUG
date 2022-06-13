f9984b8224de79acbd584e68727d4362d552edd6
s19076 <s19076@pjwstk.edu.pl>
2022-06-13 21:43:05 +0200

    added function for maxpref suff

--- utils.py
+++ utils.py
@@ -6,3 +6,22 @@ def reverse_sentence(sentence: str) -> str:
         reversed_sentence += sentence[len(sentence) - 1 - i]
 
     return reversed_sentence
+
+
+def get_max_pref_suff(sentence: str) -> str:
+    """
+    """
+    max_pref_suff = ""
+    if len(sentence) == 1:
+        return sentence
+    else:
+        for i in range(len(sentence)):
+            if sentence[i] == sentence[-1 - i]:
+                max_pref_suff += sentence[i]
+            else:
+                break
+    return max_pref_suff
+
+
+if __name__ == "__main__":
+    print(get_max_pref_suff("abaa"))
